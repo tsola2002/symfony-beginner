d19e32a304a7abe64f883455fd1e84fbddef98a7
Omatsola Sobotie <tsola2002@yahoo.co.uk>
2015-01-27 04:07:34 +0000

    adding routes & controllers

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -1,7 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: isaac
- * Date: 22/01/15
- * Time: 06:32
- */ 
+
+namespace Bookkeeper\ManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class BookController extends Controller {
+
+    public function indexAction() {
+    }
+
+    public function showAction($id) {
+    }
+
+    public function newAction() {
+    }
+
+    public function createAction(Request $request) {
+    }
+
+    public function editAction($id) {
+    }
+
+    public function updateAction(Request $request, $id) {
+    }
+
+    public function deleteAction(Request $request, $id) {
+    }
+}
