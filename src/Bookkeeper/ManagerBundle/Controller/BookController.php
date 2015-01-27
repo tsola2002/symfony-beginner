0d11bba1f278c013b2bd6e32d3c3809f1c72304e
Omatsola Sobotie <tsola2002@yahoo.co.uk>
2015-01-27 05:36:33 +0000

    added templates

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -9,18 +9,23 @@ use Symfony\Component\HttpFoundation\Request;
 class BookController extends Controller {
 
     public function indexAction() {
+        return $this->render('BookkeeperManagerBundle:Book:index.html.twig');
     }
 
     public function showAction($id) {
+        return $this->render('BookkeeperManagerBundle:Book:show.html.twig');
     }
 
     public function newAction() {
+        return $this->render('BookkeeperManagerBundle:Book:new.html.twig');
     }
 
     public function createAction(Request $request) {
+
     }
 
     public function editAction($id) {
+        return $this->render('BookkeeperManagerBundle:Book:edit.html.twig');
     }
 
     public function updateAction(Request $request, $id) {
