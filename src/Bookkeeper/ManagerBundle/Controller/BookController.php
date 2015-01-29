f149c34b12c50d7bd309c80d44a98d9f5fc26873
Omatsola Sobotie <tsola2002@yahoo.co.uk>
2015-01-29 13:51:38 +0000

    building forms & form types

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -4,6 +4,8 @@ namespace Bookkeeper\ManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Bookkeeper\ManagerBundle\Entity\Book;
+use Bookkeeper\ManagerBundle\Form\BookType;
 
 
 class BookController extends Controller {
@@ -17,7 +19,18 @@ class BookController extends Controller {
     }
 
     public function newAction() {
-        return $this->render('BookkeeperManagerBundle:Book:new.html.twig');
+        $book = new Book();
+
+        $form = $this->createForm(new BookType(), $book, array(
+            'action'=>$this->generateUrl('book_create'),
+            'method'=>'POST'
+        ));
+
+        $form->add('submit', 'submit', array('label'=>'Create Book'));
+
+        return $this->render('BookkeeperManagerBundle:Book:new.html.twig', array(
+            'form'=>$form->createView()
+        ));
     }
 
     public function createAction(Request $request) {
---
+++ src/Bookkeeper/ManagerBundle/Form/BookType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bookkeeper\ManagerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Translation\Catalogue\OperationInterface;
+
+class BookType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('title')
+                ->add('description')
+                ->add('pages');
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array('data_class'=>'Bookkeeper\ManagerBundle\Entity\Book'));
+    }
+
+    public function getName() {
+        return 'bookkeeper_managerbundle_book';
+    }
+
+} 
