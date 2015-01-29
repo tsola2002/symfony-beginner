9c22344a42b8f45f1a8ad3173444c53524ca8bbd
Omatsola Sobotie <tsola2002@yahoo.co.uk>
2015-01-29 11:18:47 +0000

    a sample greeting command in symfony

---
+++ src/Bookkeeper/ManagerBundle/Command/GreetCommand.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: isaac
+ * Date: 28/01/15
+ * Time: 11:01
+ */
+
+namespace Bookkeeper\ManagerBundle\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class GreetCommand
+ * @package Bookkeeper\ManagerBundle\Command
+ */
+class GreetCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+        $this
+            ->setName('demo:greet')
+            ->setDescription('Greet someone')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet')
+            ->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello ' .$name;
+        }
+        else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption('yell')) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+    }
+} 
